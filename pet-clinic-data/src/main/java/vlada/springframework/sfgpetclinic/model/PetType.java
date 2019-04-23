9f200090d33fefebbd6e231807ac77b1f9a12d6d
VladimirMarinkovic <exxperiment899@gmail.com>
2019-04-23 03:41:22 +0200

    adding release plugin. #3

---
+++ pet-clinic-data/src/main/java/vlada/springframework/sfgpetclinic/model/PetType.java
@@ -0,0 +1,15 @@
+package vlada.springframework.sfgpetclinic.model;
+
+public class PetType {
+
+    private String naziv;
+
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+}
