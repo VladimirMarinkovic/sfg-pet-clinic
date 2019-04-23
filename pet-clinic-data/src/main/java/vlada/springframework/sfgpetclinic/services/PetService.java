4dad58f8b1a392c474b28248fdf99fbe4496a9d9
VladimirMarinkovic <exxperiment899@gmail.com>
2019-04-23 11:30:39 +0200

    Implement Interfaces for services. Close #5

---
+++ pet-clinic-data/src/main/java/vlada/springframework/sfgpetclinic/services/PetService.java
@@ -0,0 +1,12 @@
+package vlada.springframework.sfgpetclinic.services;
+
+import vlada.springframework.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/vlada/springframework/sfgpetclinic/services/VetService.java
@@ -0,0 +1,12 @@
+package vlada.springframework.sfgpetclinic.services;
+
+import vlada.springframework.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}
