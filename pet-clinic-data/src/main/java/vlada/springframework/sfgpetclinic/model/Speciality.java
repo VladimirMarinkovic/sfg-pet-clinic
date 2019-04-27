73cf5eb99f47f868f83cdda461919b6e8ad41696
VladimirMarinkovic <exxperiment899@gmail.com>
2019-04-27 20:28:17 +0200

    Dodavanje Speciality za Vet. Closes #27

---
+++ pet-clinic-data/src/main/java/vlada/springframework/sfgpetclinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package vlada.springframework.sfgpetclinic.model;
+
+public class Speciality extends BaseEntity {
+
+    private String opis;
+
+    public String getOpis() {
+        return opis;
+    }
+
+    public void setOpis(String opis) {
+        this.opis = opis;
+    }
+}
