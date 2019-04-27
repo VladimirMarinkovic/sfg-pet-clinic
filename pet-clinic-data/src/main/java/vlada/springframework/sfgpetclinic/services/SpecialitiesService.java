b2dd2efcaca62dd95020b09f5a8a22de8fd24900
VladimirMarinkovic <exxperiment899@gmail.com>
2019-04-27 22:36:54 +0200

    Dodavanje Specialties Service Map. Closes #

---
+++ pet-clinic-data/src/main/java/vlada/springframework/sfgpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package vlada.springframework.sfgpetclinic.services;
+
+import vlada.springframework.sfgpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
