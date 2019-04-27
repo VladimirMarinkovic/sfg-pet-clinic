5ce7d619f4aed197ab7fe2884583444fbd9c1207
VladimirMarinkovic <exxperiment899@gmail.com>
2019-04-27 20:36:10 +0200

    Nadogradnja Owner. Closes #28

--- pet-clinic-data/src/main/java/vlada/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/vlada/springframework/sfgpetclinic/model/Owner.java
@@ -4,7 +4,35 @@ import java.util.Set;
 
 public class Owner extends Person {
 
-   private Set<Pet> pets;
+    private String adresa;
+    private String grad;
+    private String telefon;
+    private Set<Pet> pets;
+
+
+    public String getAdresa() {
+        return adresa;
+    }
+
+    public void setAdresa(String adresa) {
+        this.adresa = adresa;
+    }
+
+    public String getGrad() {
+        return grad;
+    }
+
+    public void setGrad(String grad) {
+        this.grad = grad;
+    }
+
+    public String getTelefon() {
+        return telefon;
+    }
+
+    public void setTelefon(String telefon) {
+        this.telefon = telefon;
+    }
 
     public Set<Pet> getPets() {
         return pets;
