fb5a521bfae2cf551ccbd7b81f7b7a68fb472561
VladimirMarinkovic <exxperiment899@gmail.com>
2019-05-01 23:25:55 +0200

    Dodate Entity anotacije i mapiranje za klase. Closes #37

--- pet-clinic-data/src/main/java/vlada/springframework/sfgpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/vlada/springframework/sfgpetclinic/model/Speciality.java
@@ -1,7 +1,15 @@
 package vlada.springframework.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "specialties")
 public class Speciality extends BaseEntity {
 
+    @Column(name = "opis")
     private String opis;
 
     public String getOpis() {
