43c8f4e4db46236a7a661a474cbc6e8637b1c008
VladimirMarinkovic <exxperiment899@gmail.com>
2019-05-01 22:45:24 +0200

    Dodata u Base Entity JPA anotacija. Closes #35

--- pet-clinic-data/src/main/java/vlada/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/vlada/springframework/sfgpetclinic/model/BaseEntity.java
@@ -1,9 +1,17 @@
 package vlada.springframework.sfgpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
@@ -13,4 +21,6 @@ public class BaseEntity implements Serializable {
     public void setId(Long id) {
         this.id = id;
     }
+
+
 }
