c53f3250d8c54b4a8ad91689e48412e3f2544987
VladimirMarinkovic <exxperiment899@gmail.com>
2019-05-15 19:29:01 +0200

    Visit Map Service implementiran. Closes #46

---
+++ pet-clinic-data/src/main/java/vlada/springframework/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,7 @@
+package vlada.springframework.sfgpetclinic.services;
+
+import vlada.springframework.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/vlada/springframework/sfgpetclinic/services/map/VisitServiceMap.java
@@ -0,0 +1,40 @@
+package vlada.springframework.sfgpetclinic.services.map;
+
+import vlada.springframework.sfgpetclinic.model.Visit;
+import vlada.springframework.sfgpetclinic.services.VisitService;
+
+import java.util.Set;
+
+public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitService {
+
+
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.finAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if (visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null
+                || visit.getPet().getOwner().getId() == null) { throw new RuntimeException("Nepostojeca poseta"); }
+        return super.save(visit);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        super.delete(visit);
+
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+
+    }
+}
